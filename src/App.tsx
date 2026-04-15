0c39594d5040ce70a4bff420153b19317b0790eb
S3EM <hagageslam430@gmail.com>
2026-04-15 14:45:23 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,11 @@ import ChatBot from './components/ChatBot';
 import Home from './pages/Home';
 import Collection from './pages/Collection';
 import Admin from './pages/Admin';
-import './index.css'; // تأكد من إضافة هذا السطر لتشغيل التصميم
+import './index.css';
 
 export default function App() {
   return (
     <Router>
-      {/* تم تعديل الـ bg والـ text لضمان توافقها مع مكتبة Tailwind */}
       <div className="flex flex-col min-h-screen font-sans text-gray-900 bg-gray-50">
         <Navbar />
         <main className="flex-grow">
